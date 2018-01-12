ca95e4c5d27ff195dc98c0d24e5c5b31b49444c6
Todd Darling <tdarling@cardinalsolutions.com>
2018-01-12 08:33:21 -0500

    Fixed typo where it said to checkout clean-urls-ssr branch. Needed to checkout with-styled-jsx branch

--- content/1-basics/8-styling-components.js
+++ content/1-basics/8-styling-components.js
@@ -31,7 +31,7 @@ In order to follow this lesson, you will need to have a simple Next.js app. For 
 ~~~bash
 git clone https://github.com/arunoda/learnnextjs-demo.git
 cd learnnextjs-demo
-git checkout clean-urls-ssr
+git checkout with-styled-jsx
 ~~~
 
 You can run it with:
